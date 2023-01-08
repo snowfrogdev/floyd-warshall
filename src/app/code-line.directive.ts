b9f14515e3bc51d5dc78bb4830b2d4361138b934
Philippe Vaillancourt <philippe.m.vaillancourt@gmail.com>
2023-01-08 04:16:12 +0100

    Add breakpoint dot visible on hover

--- src/app/code-line.directive.ts
+++ src/app/code-line.directive.ts
@@ -7,33 +7,40 @@ export class CodeLineDirective {
   @Input() appCodeLine!: number;
   @Input() lineToHighlight?: number;
 
-  private dot!: HTMLElement;
-
   constructor(private el: ElementRef, private renderer: Renderer2) {}
 
   ngOnInit() {
     const line = this.el.nativeElement;
     this.renderer.setStyle(line, 'position', 'relative');
+    const dotContainer = this.renderer.createElement('div');
+    this.renderer.addClass(dotContainer, 'breakpoint-dot-container');
+    this.renderer.setStyle(dotContainer, 'position', 'absolute');
+    this.renderer.setStyle(dotContainer, 'left', '-2.5rem');
+    this.renderer.setStyle(dotContainer, 'top', '-1px');
+    this.renderer.setStyle(dotContainer, 'cursor', 'pointer');
+    this.renderer.setStyle(dotContainer, 'position', 'absolute');
+    this.renderer.setStyle(dotContainer, 'width', '3rem');
+    this.renderer.setStyle(dotContainer, 'height', '1rem');
+    this.renderer.setStyle(dotContainer, 'display', 'flex');
+    this.renderer.setStyle(dotContainer, 'align-items', 'center');
+    this.renderer.setStyle(dotContainer, 'justify-content', 'center');
     const dot = this.renderer.createElement('div');
     this.renderer.addClass(dot, 'breakpoint-dot');
     this.renderer.setStyle(dot, 'background-color', 'red');
+    this.renderer.setStyle(dot, 'opacity', '0');
     this.renderer.setStyle(dot, 'border-radius', '50%');
     this.renderer.setStyle(dot, 'height', '0.5rem');
     this.renderer.setStyle(dot, 'width', '0.5rem');
-    this.renderer.setStyle(dot, 'position', 'absolute');
-    this.renderer.setStyle(dot, 'left', '-15px');
-    this.renderer.setStyle(dot, 'top', '4px');
-    this.renderer.setStyle(dot, 'display', 'none');
-    this.dot = dot;
-    this.renderer.appendChild(line, dot);
-  }
 
-  @HostListener('mouseenter') onMouseEnter() {
-    this.renderer.setStyle(this.dot, 'display', 'block');
-  }
+    this.renderer.appendChild(line, dotContainer);
+    this.renderer.appendChild(dotContainer, dot);
 
-  @HostListener('mouseleave') onMouseLeave() {
-    this.renderer.setStyle(this.dot, 'display', 'none');
+    this.renderer.listen(dotContainer, 'mouseenter', () => {
+      this.renderer.setStyle(dot, 'opacity', '100');
+    });
+    this.renderer.listen(dotContainer, 'mouseleave', () => {
+      this.renderer.setStyle(dot, 'opacity', '0');
+    });
   }
 
   ngOnChanges() {
